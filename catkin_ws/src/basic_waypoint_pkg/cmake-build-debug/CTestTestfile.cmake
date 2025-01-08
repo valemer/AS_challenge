a48736c5a2ab27017c727ad79806939ab6cf3d1d
Valentin Merle <valentin.merle@tum.de>
2025-01-08 17:20:18 +0100

    add planner

---
+++ catkin_ws/src/basic_waypoint_pkg/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/valentin/as/autsys-2024-trajectory-generation-valemer/catkin_ws/src/basic_waypoint_pkg
+# Build directory: /home/valentin/as/autsys-2024-trajectory-generation-valemer/catkin_ws/src/basic_waypoint_pkg/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ catkin_ws/src/basic_waypoint_pkg/cmake-build-debug/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/valentin/as/autsys-2024-trajectory-generation-valemer/catkin_ws/src/basic_waypoint_pkg/cmake-build-debug/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
