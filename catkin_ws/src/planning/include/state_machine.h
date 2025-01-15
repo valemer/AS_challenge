bc7e7ef9e48fb93e68ab23c46246839a2eaf1506
Valentin Merle <valentin.merle@tum.de>
2025-01-15 14:57:02 +0100

    add flyBack to state machine

--- catkin_ws/src/planning/include/state_machine.h
+++ catkin_ws/src/planning/include/state_machine.h
@@ -1,21 +1,17 @@
 #pragma once
 
-#include <iostream>
 #include <ros/ros.h>
-#include <Eigen/Dense>
 #include <nav_msgs/Odometry.h>
 #include <eigen_conversions/eigen_msg.h>
 #include "fla_msgs/GlobalPath.h"
-#include <mav_trajectory_generation/polynomial_optimization_nonlinear.h>
-#include <mav_trajectory_generation_ros/ros_visualization.h>
-#include <mav_trajectory_generation_ros/ros_conversions.h>
 
 enum State {
     TAKE_OFF = 0,
     GO = 1,
-    EXPLORED = 2,
+    EXPLORE = 2,
     FLY_BACK = 3,
-    STOP = 4
+    LAND = 4,
+    STOP = 5
 };
 
 class StateMachine {
@@ -26,7 +22,9 @@ public:
 
     void mainLoop(const ros::TimerEvent& t);
 
-    bool publishPath(const fla_msgs::GlobalPath& trajectory);
+    void saveWayBack();
+
+    void publishPath(const std::list<Eigen::Vector4d>& path);
 
     bool closeToGoal();
 
@@ -34,12 +32,16 @@ public:
 
     void flyToCave();
 
-    void loadAndSendPath(std::string path);
+    void loadAndSendPath(const std::string& path);
+
+    void flyBack();
+
+    void land();
 
 private:
     ros::Publisher pub_global_path_;
     ros::Subscriber sub_odom_;
-    ros::ServiceClient client;
+    ros::ServiceClient reset_octomap;
 
 
     ros::Timer timer_;
@@ -51,6 +53,13 @@ private:
 
     State state_ = TAKE_OFF;
 
-    Eigen::Vector3f current_goal_;
-    int paths_sent_ = 0;
+    std::list<Eigen::Vector4d> path_land_;
+    std::list<Eigen::Vector4d> path_back_;
+    Eigen::Vector3d current_goal_;
+    bool paths_sent_ = false;
+
+    // params
+    int time_between_states_s;
+    double min_dis_waypoint_back;
+    double max_dis_close_to_goal;
 };
--- catkin_ws/src/planning/src/state_machine.cpp
+++ catkin_ws/src/planning/src/state_machine.cpp
@@ -1,6 +1,7 @@
 #include "state_machine.h"
 
 #include <thread>
+#include <mav_msgs/common.h>
 
 #include "yaml-cpp/yaml.h"
 #include "ros/package.h"
@@ -9,7 +10,10 @@
 StateMachine::StateMachine() :
         hz_(10.0),
         current_velocity_(Eigen::Vector3d::Zero()),
-        current_pose_(Eigen::Affine3d::Identity())
+        current_pose_(Eigen::Affine3d::Identity()),
+        time_between_states_s(2),
+        min_dis_waypoint_back(5.0),
+        max_dis_close_to_goal(1.0)
 {
     // publisher
     pub_global_path_ = nh_.advertise<fla_msgs::GlobalPath>("/global_path", 0);
@@ -22,7 +26,12 @@ StateMachine::StateMachine() :
 
     // Service
     ros::service::waitForService("/octomap_server/reset");
-    client = nh_.serviceClient<std_srvs::Empty>("/octomap_server/reset");
+    reset_octomap = nh_.serviceClient<std_srvs::Empty>("/octomap_server/reset");
+
+    // params
+    nh_.getParam("state_machine/time_between_states_s", time_between_states_s);
+    nh_.getParam("state_machine/min_dis_waypoint_back", min_dis_waypoint_back);
+    nh_.getParam("state_machine/max_dis_close_to_goal", max_dis_close_to_goal);
 }
 
 // Callback to get current Pose of UAV
@@ -36,35 +45,73 @@ void StateMachine::uavOdomCallback(const nav_msgs::Odometry::ConstPtr& odom) {
 }
 
 void StateMachine::mainLoop(const ros::TimerEvent& t) {
-  switch(state_) {
-    case TAKE_OFF:
+  switch(state_)
+  {
+  case TAKE_OFF:
       takeOff();
       break;
-    case GO:
+  case GO:
       flyToCave();
       break;
+  case EXPLORE:
+      // TODO
+      saveWayBack();
+      break;
+  case FLY_BACK:
+      flyBack();
+      break;
+  case LAND:
+      land();
+      break;
+  case STOP:
+      break;
   }
+}
 
+void StateMachine::saveWayBack()
+{
+    if (sqrt(
+        pow(current_pose_.translation()[0] - path_back_.front()[0], 2)
+        + pow(current_pose_.translation()[1] - path_back_.front()[1], 2)
+        + pow(current_pose_.translation()[2] - path_back_.front()[2], 2)) > min_dis_waypoint_back)
+        {
+        path_back_.emplace_front(
+            current_pose_.translation()[0],
+            current_pose_.translation()[1],
+            current_pose_.translation()[2],
+            std::fmod(mav_msgs::yawFromQuaternion(
+            static_cast<Eigen::Quaterniond>(current_pose_.rotation())) + M_PI, 2 * M_PI));
+    }
 }
 
 bool StateMachine::closeToGoal() {
   double dist = sqrt(pow(current_pose_.translation()[0] - current_goal_[0], 2)
                      + pow(current_pose_.translation()[1] - current_goal_[1], 2)
                      + pow(current_pose_.translation()[2] - current_goal_[2], 2));
-   return dist < 0.5;
+   return dist < max_dis_close_to_goal;
 }
 
 void StateMachine::takeOff() {
-  if (paths_sent_ < 1) {
-      std::this_thread::sleep_for(std::chrono::seconds(5));
+  if (!paths_sent_) {
+      std::this_thread::sleep_for(std::chrono::seconds(time_between_states_s));
+      if (current_pose_.translation()[0] == 0
+          && current_pose_.translation()[1] == 0
+          && current_pose_.translation()[2] == 0)
+          return;
+      path_land_.emplace_front(
+            current_pose_.translation()[0],
+            current_pose_.translation()[1],
+            current_pose_.translation()[2],
+            std::fmod(mav_msgs::yawFromQuaternion(
+            static_cast<Eigen::Quaterniond>(current_pose_.rotation())) + M_PI, 2 * M_PI));
       ROS_INFO_NAMED("state_machine", "Start take-off!");
       loadAndSendPath("takeoff");
-      paths_sent_++;
+      paths_sent_ = true;
   } else if (closeToGoal()) {
     ROS_INFO_NAMED("state_machine", "Close to take-off goal!");
-    paths_sent_ = 0;
+    paths_sent_ = false;
     state_ = GO;
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::this_thread::sleep_for(std::chrono::seconds(time_between_states_s));
   }
 }
 
@@ -74,35 +121,87 @@ void StateMachine::flyToCave() {
         paths_sent_ = true;
     } else if (closeToGoal()) {
         paths_sent_ = false;
-        state_ = EXPLORED;
-        std_srvs::Empty srv;
-        if (client.call(srv)) {
-            ROS_INFO("OctoMap reset successfully.");
+        state_ = FLY_BACK;
+        /*std_srvs::Empty srv;
+        if (reset_octomap.call(srv)) {
+            ROS_INFO("OctoMap reset successful.");
         } else {
             ROS_ERROR("Failed to reset OctoMap.");
-        }
+        }*/
     }
 }
 
-void StateMachine::loadAndSendPath(std::string path) {
+void StateMachine::loadAndSendPath(const std::string& path) {
     YAML::Node config = YAML::LoadFile(ros::package::getPath("planning") +
         "/config/state_machine_config.yaml");
 
     fla_msgs::GlobalPath global_path;
     fla_msgs::GlobalPoint global_point;
-    for (const auto& point : config[path]) {
-        global_point.point.x = point[0].as<float>();
-        global_point.point.y = point[1].as<float>();
-        global_point.point.z = point[2].as<float>();
-        global_point.orientation = point[3].as<float>();
-        global_point.velocity = point[4].as<float>();
-        global_point.acceleration = point[5].as<float>();
+
+    for (auto it = config[path].begin(); it != config[path].end(); ++it) {
+        global_point.point.x = (*it)[0].as<float>();
+        global_point.point.y = (*it)[1].as<float>();
+        global_point.point.z = (*it)[2].as<float>();
+        global_point.orientation = (*it)[3].as<float>();
+        global_point.velocity = (*it)[4].as<float>();
+        global_point.acceleration = (*it)[5].as<float>();
         global_path.points.push_back(global_point);
+
+        if (path == "takeoff") {
+            if (std::next(it) != config[path].end())
+                path_land_.emplace_front((*it)[0].as<float>(), (*it)[1].as<float>(), (*it)[2].as<float>(),
+                    std::fmod((*it)[3].as<float>() + M_PI, 2 * M_PI));
+            else
+                path_back_.emplace_front((*it)[0].as<float>(), (*it)[1].as<float>(), (*it)[2].as<float>(),
+                    std::fmod((*it)[3].as<float>() + M_PI, 2 * M_PI));
+        }
+
+
+        if (path == "flyToCave" && std::next(it) != config[path].end())
+            path_back_.emplace_front((*it)[0].as<float>(), (*it)[1].as<float>(), (*it)[2].as<float>(),
+                std::fmod((*it)[3].as<float>() + M_PI, 2 * M_PI));
     }
     pub_global_path_.publish(global_path);
 
     current_goal_ << global_point.point.x, global_point.point.y, global_point.point.z;
 }
+
+void StateMachine::flyBack() {
+    if (!paths_sent_) {
+        publishPath(path_back_);
+    } else if (closeToGoal()) {
+        paths_sent_ = false;
+        state_ = LAND;
+    }
+}
+
+void StateMachine::land() {
+    if (!paths_sent_) {
+        publishPath(path_land_);
+    } else if (closeToGoal()) {
+        paths_sent_ = false;
+        state_ = STOP;
+    }
+}
+
+void StateMachine::publishPath(const std::list<Eigen::Vector4d>& path) {
+    fla_msgs::GlobalPath global_path;
+    for (const auto& point : path) {
+        fla_msgs::GlobalPoint global_point;
+        global_point.point.x = point[0];
+        global_point.point.y = point[1];
+        global_point.point.z = point[2];
+        global_point.orientation = static_cast<float>(point[3]);
+        global_point.velocity = -1.0;
+        global_point.acceleration = -1.0;
+        global_path.points.push_back(global_point);
+    }
+    pub_global_path_.publish(global_path);
+    paths_sent_ = true;
+
+    current_goal_ << global_path.points.back().point.x, global_path.points.back().point.y, global_path.points.back().point.z;
+}
+
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "state_machine");
